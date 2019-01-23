40130ceea6d1c76ee213f41ace465c31cd4fb4c7
Shankar Ram <shankar.ram@aspiresys.com>
2019-01-23 16:00:02 +0530

    Initial commit for user service

---
+++ user-service/src/main/java/com/gmail/shankar2k5/eshop/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.shankar2k5.eshop.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
