277e62ccab15117f49ec9ff7139b60085d878690
Shankar Ram <shankar.ram@aspiresys.com>
2019-01-23 14:57:38 +0530

    Updates to gitignore Initial commit of service discovery project

---
+++ service-discovery/src/main/java/com/gmail/shankar2k5/eshop/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.shankar2k5.eshop.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
