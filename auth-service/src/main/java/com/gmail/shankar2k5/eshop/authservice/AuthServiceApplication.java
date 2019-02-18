f52c4fb3104d639b1bcd4c23a3db5fe4589ba365
Shankar Ram <shankar.ram@aspiresys.com>
2019-02-18 14:11:22 +0530

    Changed user service port, added some imports

--- auth-service/src/main/java/com/gmail/shankar2k5/eshop/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/gmail/shankar2k5/eshop/authservice/AuthServiceApplication.java
@@ -2,6 +2,8 @@ package com.gmail.shankar2k5.eshop.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
